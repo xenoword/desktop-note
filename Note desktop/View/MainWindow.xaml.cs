01aee9a7a815c8022ae671e1a3f2dbb00847fa85
mortierq <mortierq@etu.univ-smb.fr>
2022-04-03 14:08:18 +0200

    test correction bug vs

--- Note desktop/View/MainWindow.xaml.cs
+++ Note desktop/View/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace Note_desktop.View
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
@@ -26,12 +27,9 @@ namespace Note_desktop.View
 
         #region WindowUpdate
 
-        private bool CanMove => Mouse.LeftButton == MouseButtonState.Pressed;
-        private bool CanResize { get; set; }
-        private bool IsOverMainGrid => MainGrid.IsMouseOver && !NoteListView.IsMouseOver && !BtnAdd.IsMouseOver && !BtnClose.IsMouseOver;// && !BtnReduce.IsMouseOver;
-
         private void this_LocationChanged(object sender, EventArgs e)
         {
+            //Top = Top > SystemParameters.PrimaryScreenHeight - MinHeight - 20 ? Top = SystemParameters.PrimaryScreenHeight - MinHeight - 20 : Top;
             if (Top > SystemParameters.PrimaryScreenHeight - MinHeight - 20)
             {
                 Top = SystemParameters.PrimaryScreenHeight - MinHeight - 20;
@@ -39,18 +37,84 @@ namespace Note_desktop.View
             MaxHeight = SystemParameters.PrimaryScreenHeight - Top - 10;
         }
 
-        private void MainGrid_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (CanMove && IsOverMainGrid)
-            {
-                DragMove();
-            }
-        }
         #endregion
 
-        private void this_StateChanged(object sender, EventArgs e)
+        #region Test
+        private void header_Loaded(object sender, RoutedEventArgs e)
         {
-            this.WindowState = WindowState.Normal;
+            InitHeader(sender as Grid);
         }
+        private void InitHeader(Grid header)
+        {
+            var restoreIfMove = false;
+
+            header.MouseLeftButtonDown += (s, e) =>
+            {
+                if (e.ClickCount == 2)
+                {
+                    if ((ResizeMode == ResizeMode.CanResize) ||
+                        (ResizeMode == ResizeMode.CanResizeWithGrip))
+                    {
+                        SwitchState();
+                    }
+                }
+                else
+                {
+                    if (WindowState == WindowState.Maximized)
+                    {
+                        restoreIfMove = true;
+                    }
+
+                    DragMove();
+                }
+            };
+            header.MouseLeftButtonUp += (s, e) =>
+            {
+                restoreIfMove = false;
+            };
+            header.MouseMove += (s, e) =>
+            {
+                if (restoreIfMove)
+                {
+                    restoreIfMove = false;
+                    var mouseX = e.GetPosition(this).X;
+                    var width = RestoreBounds.Width;
+                    var x = mouseX - width / 2;
+
+                    if (x < 0)
+                    {
+                        x = 0;
+                    }
+                    else
+                    if (x + width > SystemParameters.PrimaryScreenWidth)
+                    {
+                        x = SystemParameters.PrimaryScreenWidth - width;
+                    }
+
+                    WindowState = WindowState.Normal;
+                    Left = x;
+                    Top = 0;
+                    DragMove();
+                }
+            };
+        }
+        private void SwitchState()
+        {
+            switch (WindowState)
+            {
+                case WindowState.Normal:
+                    {
+                        WindowState = WindowState.Maximized;
+                        break;
+                    }
+                case WindowState.Maximized:
+                    {
+                        WindowState = WindowState.Normal;
+                        break;
+                    }
+            }
+        }
+
+        #endregion
     }
 }
